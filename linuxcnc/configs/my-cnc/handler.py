1d5aee75d40b549356078941c45c4c159d6de931
panhandle76 <panhandle76@gmail.com>
2020-05-15 23:51:35 -0400

    fixed up some code to properly wait for gcode complete. made some movements incremental. deleted the pyc file.

--- linuxcnc/configs/my-cnc/handler.py
+++ linuxcnc/configs/my-cnc/handler.py
@@ -6,7 +6,9 @@ import linuxcnc
 import gtk
 import gtk.glade
 
-
+###
+### Just for prompting user to do something
+###
 class BlockingWindow():
    def __init__(self, message):
       self.gladefile = "prompt.ui"
@@ -27,6 +29,9 @@ class BlockingWindow():
       return None
 
 
+###
+### Prompts user for cornerfinder/zero parameters
+###
 class AutoToolZero:
    def __init__(self):
       self.gladefile = "autotoolzero.ui"
@@ -78,19 +83,40 @@ class AutoToolZero:
       return {"x":self.doX, "y":self.doY, "position":self.position, "pause":self.pause, "radius":self.radius, "unit":self.unit}
 
 
+###
+### Main class for handling button presses on the main screen
+###
 class HandlerClass:
 
    def __init__(self, halcomp, builder, useropts):
       print("init")
       #self.halcomp = halcomp
       #self.halcomp.newpin("number", hal.HAL_FLOAT, hal.HAL_IN)
+      
+      # put in MDI mode
+      self.c = linuxcnc.command()
+      self.c.mode(linuxcnc.MODE_MDI)
+      self.c.wait_complete(30) # wait for mode switch (max 30 seconds)
+
       self.touch_plate_height = 1                       # height of touchplate (inch)
       self.touch_plate_width = 2.205                    # width of touchplate (inch) (~56mm)
       self.ztravel_height = 0.125                       # how high to lift tool while probing X and Y (inch)
       self.zlift_height = 0.5                           # how high to lift after probing is done (inch)
 
+   def run_mdi(self, command):
+      self.c.mdi(command)
+      self.c.wait_complete(30)  # wait for 30 seconds
+
+   def move_abs(self, command, axis, feed):
+      self.run_mdi("G90")  # absolute
+      self.run_mdi(command+" "+axis+" "+feed)
+
+   def move_inc(self, command, axis, feed):
+      self.run_mdi("G91")  # incremental
+      self.run_mdi(command+" "+axis+" "+feed)
+
    def on_btnAutoToolZero_clicked(self, button):
-      # prompt user with dialog
+      # prompt user with dialog and get the results
       atz = AutoToolZero()
       values = atz.run()
       print("values="+str(values))
@@ -99,67 +125,47 @@ class HandlerClass:
       radius = values["radius"]
       unit = values["unit"]
 
-      c = linuxcnc.command()
-      c.mode(linuxcnc.MODE_MDI)
-      c.wait_complete() # wait for mode switch
+      #
+      self.run_mdi("G20") # inch
 
-      ### set units to inch
-      c.mdi("G20")
-      c.wait_complete() # wait
-      ###
-      c.mdi("G90")  # absolute
-      c.wait_complete() # wait
-
-      ### probe Z
+      # probe Z
       b = BlockingWindow("Jog tool over touchplate and Press Ok")
       b.run()
-      self.probeZ("Z", radius)
+      self.probeZ("Z")
 
-      ### probe X if requested
+      # probe X if requested
       if doX:
          b = BlockingWindow("Align Tool Flutes for X-Axis Travel and Press Ok")
          b.run()
          self.probeXY("X", radius)
 
-      ### probe Y if requested
+      # probe Y if requested
       if doY:
          b = BlockingWindow("Align Tool Flutes for Y-Axis Travel and Press Ok")
          b.run()
          self.probeXY("Y", radius)
 
-      ### move up enough to clear the tool
-      c.mdi("G1 Z" + str(1+self.zlift_height) + " F2")
-      c.wait_complete() # wait
+      # move up enough to clear the tool
+      self.move_inc("G1", "Z"+str(self.zlift_height), "F2")
 
-   def probeZ(self, axis, tool_radius):
-      print("probe " + axis + ":" + str(tool_radius));
-      c = linuxcnc.command()
-      c.mode(linuxcnc.MODE_MDI)
-      c.wait_complete() # wait for mode switch
-      ### move until we hit the probe
-      c.mdi("G38.3 Z-4 F2")
-      c.wait_complete() # wait
-      ### set the Z offset. This gcode will calculate the necessary Z offset (G54) for a final position of '1'
-      c.mdi("G10 L20 P1 Z1")
-      c.wait_complete() # wait
-      ### move up a little
-      c.mdi("G1 Z" + str(1+self.ztravel_height) + " F2")
-      c.wait_complete() # wait
+   def probeZ(self, axis):
+      print("probe " + axis);
+      # move until we hit the probe
+      self.run_mdi("G38.3 Z-4 F2")
+      # set the Z offset. This gcode will calculate the necessary Z offset (G54) for a final position of '1'
+      self.run_mdi("G10 L20 P1 Z1")
+      # move up a little
+      self.move_inc("G1", "Z"+str(self.ztravel_height), "F2")
 
    def probeXY(self, axis, tool_radius):
       print("probe " + axis + ":" + str(tool_radius));
-      c = linuxcnc.command()
-      c.mode(linuxcnc.MODE_MDI)
-      c.wait_complete() # wait for mode switch
-      ### move toward probe and stop
-      c.mdi("G38.3 "+axis+"15 F2")  # 15 is just some large number to move toward. we should hit touch plate before then
-      c.wait_complete() # wait
-      ### set offset to be (touchprobe width minus tool radius)
-      c.mdi("G10 L20 P1 " + axis + str(self.touch_plate_width-tool_radius))
-      c.wait_complete() # wait
-      ### move back roughly to the center
-      c.mdi("G54 G1 " + axis + "1" + " F10")
-      c.wait_complete() # wait
+      self.run_mdi("G91")  # incremental
+      # move toward probe and stop
+      self.run_mdi("G38.3 "+axis+"2 F2")  # 15 is just some large number to move toward. we should hit touch plate before then
+      # set offset to be (touchprobe width minus tool radius)
+      self.run_mdi("G10 L20 P1 " + axis + str(self.touch_plate_width-tool_radius))
+      # move back roughly to the center
+      self.move_inc("G54 G1", axis+"-1", "F10")
 
 def get_handlers(halcomp, builder, useropts):
    return [HandlerClass(halcomp,builder,useropts)]
